658e1ce8bccd993cd76217a9bb22469b81ff5607
Wellington2708 <wellingtondevelopercontact@gmail.com>
2022-10-20 20:49:26 +0100

    Adicionando arquivos do projeto

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const robotron= document.querySelector("#robotron");
+
+robotron.addEventListener("click" , (evento) => {
+    console.log(evento);
+})
+
+function dizOi() {
+    console.log("Oi");
+    console.log("Bem vindo ao Robotron 2000.");
+}
+
+dizOi("Pedro");
